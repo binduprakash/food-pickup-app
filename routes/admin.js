ce54a13ee0a1288410ec535acc81ef90869256c1
Bindu Prakash <bindu2305@gmail.com>
2019-03-01 05:52:32 +0000

    fixed admin route by removing redundancy

--- routes/admin.js
+++ routes/admin.js
@@ -9,13 +9,13 @@ module.exports = function() {
     adminRoutes.get("/orders", (req, res) => {
         res.render("admin_orders");
     });
-    
+
     //Admin Order ID Page
-    adminRoutes.get('/admin/orders/:orderID', (req, res) => {
+    adminRoutes.get('/orders/:orderID', (req, res) => {
         res.render("admin_order_edit");
     });
-    
-    adminRoutes.post('/admin/orders/:orderID/edit', (req, res) => {
+
+    adminRoutes.post('/orders/:orderID', (req, res) => {
         res.render("admin_order_edit");
     });
     return adminRoutes;
