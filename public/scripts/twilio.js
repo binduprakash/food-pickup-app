b8d9bf0ba4d3ffcf6cc83ed24f045619f721e86c
Tyler <tyler.tomczyk@gmail.com>
2019-03-01 06:20:33 +0000

    setting up twilio function

--- public/scripts/twilio.js
+++ public/scripts/twilio.js
@@ -1,15 +1,30 @@
 //secrets file containing token and phone numbers
-const secrets = require('./secrets');
 
-var accountSid = secrets.twilioAccountSid; // Your Account SID from www.twilio.com/console
-var authToken = secrets.twilioAuthToken;   // Your Auth Token from www.twilio.com/console
+const twilio = function(stringMsg, phone) {
 
-var twilio = require('twilio');
-var client = new twilio(accountSid, authToken);
+    console.log(stringMsg, phone)
 
-client.messages.create({
-    body: 'Hello from Node',
-    to: secrets.twilioTo,  // Text this number
-    from: secrets.twilioFrom // From a valid Twilio number
-})
-.then((message) => console.log(message.sid));
+    
+    const secrets = require('./secrets');
+
+    var accountSid = secrets.twilioAccountSid; // Your Account SID from www.twilio.com/console
+    var authToken = secrets.twilioAuthToken;   // Your Auth Token from www.twilio.com/console
+
+    var twilio = require('twilio');
+    var client = new twilio(accountSid, authToken);
+
+    client.messages.create({
+        body: stringMsg,
+        to: phone,  // Text this number
+        from: secrets.twilioFrom // From a valid Twilio number
+    })
+    .then((message) => console.log(message.sid));
+
+}
+
+let time = "30 minutes";
+let phoneNumber = "+17789274265"
+
+let stringMessage = `Your order will be ready in ${time}`
+
+twilio(stringMessage, phoneNumber)
