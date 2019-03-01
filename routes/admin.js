8acb361407dfb3829482f2873df273bdde85e2b6
Bindu Prakash <bindu2305@gmail.com>
2019-03-01 21:14:54 +0000

    api added for order details

--- routes/admin.js
+++ routes/admin.js
@@ -17,6 +17,20 @@ module.exports = function(knex) {
       });
     });
 
+    adminRoutes.get("/orders_details_json/:orderID", (req, res) => {
+      knex
+      .select("orders.id", "order_status.name", "orders.created_at", "orders.customer_first_name", "orders.customer_phone_number", "order_menu_items.menu_items_id", "order_menu_items.quantity", "menu_items.name", "menu_items.price", "orders.total_cost")
+      .from("orders")
+      .where({"orders.id": req.params.orderID})
+      .innerJoin('order_status', 'orders.status_id', 'order_status.id')
+      .innerJoin('order_menu_items', 'orders.id', 'order_menu_items.order_id')
+      .innerJoin('menu_items', 'order_menu_items.menu_items_id', 'menu_items.id')
+      .then(results => {
+        console.log("results appear", results);
+        res.json(results);
+      });
+    });
+
     // Admin Orders Page
     adminRoutes.get("/orders", (req, res) => {
         res.render("admin_orders");
