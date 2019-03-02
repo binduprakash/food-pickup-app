b181cac4efca9e57c4750f526b4724b9a649d5e7
Bindu Prakash <bindu2305@gmail.com>
2019-03-02 08:22:51 +0000

    added id for order status seeding

--- db/seeds/02_orders.js
+++ db/seeds/02_orders.js
@@ -4,10 +4,10 @@ exports.seed = function(knex, Promise) {
     .then(function() {
       return knex.raw("ALTER SEQUENCE IF EXISTS order_status_id_seq RESTART 1").then(function() {
         return Promise.all([
-          knex("order_status").insert({ name: "new" }),
-          knex("order_status").insert({ name: "in_progress" }),
-          knex("order_status").insert({ name: "completed" }),
-          knex("order_status").insert({ name: "pending" })
+          knex("order_status").insert({ id:1, name: "new" }),
+          knex("order_status").insert({ id:2, name: "in_progress" }),
+          knex("order_status").insert({ id:3, name: "completed" }),
+          knex("order_status").insert({ id:4, name: "pending" })
         ]).then(function() {
           return knex("order_menu_items")
             .del()
