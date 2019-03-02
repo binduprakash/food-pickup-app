48f2c6323ea61d299887e80f6cdd3610cdf43e44
Bindu Prakash <bindu2305@gmail.com>
2019-03-02 08:21:52 +0000

    added twilio and minor fixes

--- public/scripts/admin.js
+++ public/scripts/admin.js
@@ -6,25 +6,6 @@ function loadOrders() {
   );
 }
 
-function getFormattedTime(createdAt){
-  let today = new Date();
-  createdAt = new Date(createdAt);
-  let timeDiffInSeconds = (today.getTime() - createdAt.getTime()) / 1000;
-  if (timeDiffInSeconds < 1){
-    return 'Just now';
-  } else if (timeDiffInSeconds < 60) {
-    return Math.round(timeDiffInSeconds) + ' sec ago';
-  } else if ((timeDiffInSeconds / 60) < 60){
-    return Math.round(timeDiffInSeconds / 60) + ' mins ago';
-  } else if (((timeDiffInSeconds / 60)/60) < 24){
-    return Math.round((timeDiffInSeconds / 60)/60) + ' hrs ago';
-  } else if ((((timeDiffInSeconds / 60)/60)/24) < 27){
-    return Math.round(((timeDiffInSeconds / 60)/60)/24) + ' days ago';
-  } else {
-    return createdAt.toLocaleString();
-  }
-}
-
 function renderOrders(orders) {
   orders.forEach(function(order){
     var $order = createOrderElement(order);
@@ -32,7 +13,7 @@ function renderOrders(orders) {
       $('#new_orders ul.order_type').append($order);
     } else if(order.name == 'in_progress'){
       $('#in_progress_orders ul.order_type').append($order);
-    } else {
+    } else if(order.name == 'completed'){
       $('#completed_orders ul.order_type').append($order);
     }
   });
@@ -44,7 +25,6 @@ function createOrderElement(order){
   let $aTag = $("<a>").attr("href", "/admin/order_edit/?order_id=" + order.id);
   $aTag.text(order.id + ' - ' + order.customer_first_name);
   $liTag.append($aTag);
-  $liTag.append($("<span>").text(' - ' + getFormattedTime(order.created_at)));
   return $liTag;
 }
 
